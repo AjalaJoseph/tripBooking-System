dcf5d5404c2944a717109d051f3ed072ea28a5d1
AjalaJoseph <ajalaoluwafikayomi27@gmail.com>
2026-08-26 19:44:23 +0100

    Update authController.ts

--- BMS/src/controllers/authController.ts
+++ BMS/src/controllers/authController.ts
@@ -112,7 +112,7 @@ export const handleStaffLogin = async (req: Request, res: Response, next: NextFu
       httpOnly: true,
       path:"/",
       secure: process.env.NODE_ENV === "production", // Active HTTPS only in production
-       sameSite: "none",
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
       partitioned: process.env.NODE_ENV === "production"
     });
