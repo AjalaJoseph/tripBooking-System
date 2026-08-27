ae8238004492722896692ab1b2ef3bc67532384c
AjalaJoseph <ajalaoluwafikayomi27@gmail.com>
2026-08-27 02:51:41 +0100

    change email transportal

--- BMS/src/backgroundJobs/emailJobs.ts
+++ BMS/src/backgroundJobs/emailJobs.ts
@@ -1,18 +1,25 @@
 import { transporter } from "../config/nodeMailer";
 import dotenv from "dotenv"
+import resend from "../config/resend";
 import { staffWelcomeEmailContent } from "../mails/sendWelcomeEmailToStaff";
 import { generateStaffUpdateProfile } from "../mails/updateProfileMail";
 import { passwordResetOtpEmailContent } from "../mails/resetPassword";
 dotenv.config()
 export const sendStaffWelcomeEmail = async (data:any)=>{
     // console.log("email job",data)
-    await transporter.sendMail({
-         from: '"Baazio No-Reply" <noreply@Baazio.System>', 
-        to: data.staff_email,
-        subject: '🚀 Welcome to the Team - Your Baazio Workstation Access Credentials',
-        text: `Hello ${data.staff_name}, your Baazio login Email is: ${data.staff_email} and temporary password is: ${data.password}.`,
-        html: staffWelcomeEmailContent(data)
+    await resend.emails.send({
+      from:'"Baazio No-Reply" <noreply@Baazio.System>',
+      to:data.staff_email,
+      subject:'🚀 Welcome to the Team - Your Baazio Workstation Access Credentials',
+      html:staffWelcomeEmailContent(data)
     })
+    // await transporter.sendMail({
+    //      from: '"Baazio No-Reply" <noreply@Baazio.System>', 
+    //     to: data.staff_email,
+    //     subject: '🚀 Welcome to the Team - Your Baazio Workstation Access Credentials',
+    //     text: `Hello ${data.staff_name}, your Baazio login Email is: ${data.staff_email} and temporary password is: ${data.password}.`,
+    //     html: staffWelcomeEmailContent(data)
+    // })
 }
 
 //  update profile email
